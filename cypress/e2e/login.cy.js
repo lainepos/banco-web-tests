318fd4763b052c79f19a15fe1c55c145460a2685
laine <lainepos@gmail.com>
2025-10-12 19:57:10 -0300

    adicionado screen a execução de testes

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,17 +1,19 @@
 describe('Login', () => {
   beforeEach( () => {
     cy.visit('http://localhost:4000')
-
+    cy.screenshot(' apos-acessar-a-pagina-de-login')
   })
 
 
-  it('Login com dados válidos deve permitir entrada no sistema', () => {
+  it.only('Login com dados válidos deve permitir entrada no sistema', () => {
     
     cy.get('#username').click()
     cy.get('#username').type('julio.lima')
     cy.get('#senha').click()
     cy.get('#senha').type('123456')
+    cy.screenshot(' apos-preencher-dados-validos')
     cy.get('#login-section > .btn').click()
+    cy.screenshot(' apos-clicar-no-botao-entrar')
 
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
 
